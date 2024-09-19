69daa2ba3ff01b91fae04269eb2027e796d1ff5d
arsmoriendy <nugrahasteven@gmail.com>
2024-09-19 17:07:11 +0700

    feat: implement generic binary search

--- internal/binarysearch.go
+++ internal/binarysearch.go
@@ -30,3 +30,51 @@ func bs(arr *[]int, toFind int, startIdx uint, endIdx uint, pivotIdx uint) (bool
 		return true, pivotIdx
 	}
 }
+
+type Compareable interface {
+	// HACK: param should be the same type as self
+	Larger(*Compareable) (bool, error)
+	// HACK: param should be the same type as self
+	Smaller(*Compareable) (bool, error)
+	// HACK: param should be the same type as self
+	Equal(*Compareable) (bool, error)
+}
+
+func BinarySearchC(arr *[]Compareable, toFind *Compareable) (bool, uint, error) {
+	n := uint(len(*arr))
+	if n == 0 {
+		return false, 0, ErrSearchEmptyArr
+	}
+
+	return bsc(arr, toFind, 0, n-1, n/2)
+}
+
+func bsc(arr *[]Compareable, toFind *Compareable, startIdx uint, endIdx uint, pivotIdx uint) (bool, uint, error) {
+	pivotVal := &(*arr)[pivotIdx]
+
+	isLarger, err := (*pivotVal).Larger(toFind)
+	if err != nil {
+		return false, 0, err
+	}
+	isSmaller, err := (*pivotVal).Smaller(toFind)
+	if err != nil {
+		return false, 0, err
+	}
+	isEqual, err := (*pivotVal).Equal(toFind)
+	if err != nil {
+		return false, 0, err
+	}
+
+	if endIdx-startIdx < 2 {
+		return isEqual, pivotIdx, nil
+	}
+
+	if isLarger {
+		return bsc(arr, toFind, startIdx, pivotIdx, startIdx+(pivotIdx-startIdx)/2)
+	} else if isSmaller {
+		pivotIdx++
+		return bsc(arr, toFind, pivotIdx, endIdx, pivotIdx+(endIdx-pivotIdx)/2)
+	} else {
+		return true, pivotIdx, nil
+	}
+}
