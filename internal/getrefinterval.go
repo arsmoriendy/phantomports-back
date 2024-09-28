b9e12a5fa74968780e70db2c3767bcd709b49f05
arsmoriendy <nugrahasteven@gmail.com>
2024-09-28 16:07:50 +0700

    feat: init dotenv before getting refresh interval

--- internal/getrefinterval.go
+++ internal/getrefinterval.go
@@ -4,11 +4,14 @@ import (
 	"os"
 	"strconv"
 	"time"
+
+	"github.com/joho/godotenv"
 )
 
 var RefInterval = getRefInterval()
 
 func getRefInterval() (ri time.Duration) {
+	godotenv.Load()
 	ri_env := os.Getenv("REFRESH_INTERVAL")
 	if ri_int, err := strconv.Atoi(ri_env); err != nil {
 		ri = time.Hour
