645caf1c2dde096913e30895f91d0926eee6ab16
arsmoriendy <nugrahasteven@gmail.com>
2024-09-28 15:47:44 +0700

    feat: add db checks

--- db/db_test.go
+++ db/db_test.go
@@ -1,8 +1,11 @@
 package db
 
 import (
+	"os"
 	"testing"
+	"time"
 
+	"github.com/arsmoriendy/opor/gql-srv/internal"
 	"github.com/joho/godotenv"
 )
 
@@ -38,6 +41,98 @@ func TestNewFrontUuid(t *testing.T) {
 	uuid, err := NewFrontUuid()
 	if err != nil {
 		t.Fatal(err)
+	}
+
+	err = RmUuid(uuid)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestUuidValid(t *testing.T) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	InitPool()
+	defer Pool.Close()
+
+	uuid, err := NewUuid()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	valid, err := UuidValid(uuid)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !valid {
+		t.Fatal(valid)
+	}
+
+	err = RmUuid(uuid)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestValidFrontUuid(t *testing.T) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	InitPool()
+	defer Pool.Close()
+
+	uuid, err := NewFrontUuid()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	valid, err := UuidValid(uuid)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !valid {
+		t.Fatal(valid)
+	}
+
+	err = RmUuid(uuid)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestExpiredFrontUuid(t *testing.T) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	os.Setenv("FRONT_UUID_EXPR", "1")
+	internal.ResetFrontUuidExpr()
+
+	InitPool()
+	defer Pool.Close()
+
+	uuid, err := NewFrontUuid()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	time.Sleep(internal.FrontUuidExpr)
+
+	valid, err := UuidValid(uuid)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if valid {
+		t.Fatal(valid)
 	}
 
 	err = RmUuid(uuid)
