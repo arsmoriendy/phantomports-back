35d10d15aaf525e40edf6de883ad6b2e9201deb1
arsmoriendy <nugrahasteven@gmail.com>
2024-10-18 15:49:43 +0700

    feat: test unregistered uuid

--- db/db_test.go
+++ db/db_test.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/arsmoriendy/opor/gql-srv/internal"
+	"github.com/google/uuid"
 	"github.com/joho/godotenv"
 )
 
@@ -126,6 +127,32 @@ func TestExpiredFrontUuid(t *testing.T) {
 		t.Fail()
 	}
 	if err != nil && !errors.Is(err, ErrExpiredUuid) {
+		t.Fatal(err)
+	}
+
+	err = RmUuid(uuid)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestUnregisteredUuid(t *testing.T) {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		panic(err)
+	}
+
+	InitPool()
+	defer Pool.Close()
+
+	uuid := uuid.New().String()
+
+	err = UuidValid(uuid)
+	if err == nil {
+		t.Fail()
+	}
+
+	if !errors.Is(err, ErrUnregisteredUuid) {
 		t.Fatal(err)
 	}
 
